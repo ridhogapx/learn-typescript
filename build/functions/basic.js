a3af037d85841036f5650d772e8f2f271cad19c3
RageNeko26 <rneko2006@gmail.com>
2023-05-03 22:19:18 +0700

    Basic function code sample

--- build/functions/basic.js
+++ build/functions/basic.js
@@ -1,1 +1,9 @@
 "use strict";
+/*
+Define function called multipleNum that takes an n variable of type number
+and returns a number
+ */
+function multipleNum(n) {
+    return n * n;
+}
+console.log(multipleNum(5));
---
+++ functions/basic.ts
@@ -0,0 +1,10 @@
+/*
+Define function called multipleNum that takes an n variable of type number 
+and returns a number
+ */
+
+ function multipleNum(n: number): number {
+ 	return n * n;
+ }
+
+ console.log(multipleNum(5));
