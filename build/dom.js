b70dd134f6e1943aa5124373b41ac2b7313706ef
RageNeko26 <rneko2006@gmail.com>
2023-05-04 00:26:10 +0700

    Manipulating DOM using Typescript

---
+++ build/dom.js
@@ -0,0 +1,5 @@
+"use strict";
+/* Some example how we interact DOM using Typescript */
+const submitBtn = document.getElementById('btn');
+const container = document.getElementById('container');
+const header = document.querySelector('h1');
---
+++ dom.ts
@@ -0,0 +1,5 @@
+/* Some example how we interact DOM using Typescript */
+const submitBtn = document.getElementById('btn') as HTMLButtonElement;
+const container = document.getElementById('container') as HTMLDivElement;
+
+const header = document.querySelector('h1') as HTMLHeadElement;
