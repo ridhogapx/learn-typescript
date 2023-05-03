be996e86449833efb4c927ee261140ae5de726b3
RageNeko26 <rneko2006@gmail.com>
2023-05-03 11:18:31 +0700

    Adding some example

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var sports = 'football';
-var id = 5;
+"use strict";
+/* 	Let's learning!  */
+/* Vanilla Javascript */
+let sports = 'football';
+let id = 5;
+/* Set data type using ':' after declaring a variable */
+let person = 'John';
+let phoneNumber = 62898989989;
--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
-/* Testing  */
+/* 	Let's learning!  */
+/* Vanilla Javascript */
 let sports = 'football';
-let id = 5
+let id = 5;
+
+/* Set data type using ':' after declaring a variable */
+let person: string = 'John';
+let phoneNumber: number = 62898989989;
+
 
