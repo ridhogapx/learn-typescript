0898049eff59ee028d94da7c8066b345189d8e48
RageNeko26 <rneko2006@gmail.com>
2023-05-04 09:45:46 +0700

    Updating Interface Typescript

--- build/interface-advance.js
+++ build/interface-advance.js
@@ -1,1 +1,9 @@
 "use strict";
+function profile(person) {
+    console.log(`His name is ${person.name}. He's ${person.age}`);
+}
+profile({
+    name: 'Ridho',
+    age: 17,
+    isMale: true
+});
--- interface-advance.ts
+++ interface-advance.ts
@@ -4,3 +4,14 @@ interface Profession {
 	isMale: boolean;
 }
 
+function profile(person: Profession):void {
+	console.log(`His name is ${person.name}. He's ${person.age}`);
+
+}
+
+profile({
+	name: 'Ridho',
+	age: 17,
+	isMale: true
+})
+
